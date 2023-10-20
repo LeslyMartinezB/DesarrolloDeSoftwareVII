1e7caaa3df4330677b732b95c4a99e713bc8366d
Lesly <leslymbonet@gmail.com>
2023-10-20 17:24:36 -0500

    1: eliminando laboratorio 8

--- lab8/lab82.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'class_lib.php';
-
-if (isset($_POST['numero'])) {
-    $valor = $_POST['numero'];
-    $arregloObjeto = new Arreglo($valor);
-    $arregloObjeto->imprimirArreglo();
-}
-?>
